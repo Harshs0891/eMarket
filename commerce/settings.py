df5a89c71a603580e16c73d326a38f88bbbbfdab
Sarthak Kumar Singh <119841283+Sarthacker@users.noreply.github.com>
2024-12-22 16:47:16 +0530

    Update settings.py

--- commerce/settings.py
+++ commerce/settings.py
@@ -23,7 +23,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '6ps8j!crjgrxt34cqbqn7x&b3y%(fny8k8nh21+qa)%ws3fh!q'
+SECRET_KEY = 'your_key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
